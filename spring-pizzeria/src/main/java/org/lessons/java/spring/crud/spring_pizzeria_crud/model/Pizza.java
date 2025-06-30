7a1ff21a08c80f7e156210a2a0b59da4e99cf3a2
AleB-135 <alessiobrusca1999@outlook.com>
2025-06-30 10:18:08 +0200

    feat: Aggiunto tasto offerta speciale

---
+++ spring-pizzeria/src/main/java/org/lessons/java/spring/crud/spring_pizzeria_crud/model/Pizza.java
@@ -0,0 +1,106 @@
+package org.lessons.java.spring.crud.spring_pizzeria_crud.model;
+
+import java.io.Serializable;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+
+@Entity
+@Table(name = "pizze")
+public class Pizza implements Serializable{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = "pizza_name", nullable = false)
+
+    @NotBlank(message = "Name cannot be null, blank or empty")
+    private String name;
+
+    @NotBlank(message = "Description cannot be null, blank or empty")
+    @Size(max = 2000, message = "Description cannot be longer than 2000 characters")
+    private String description;
+
+    // @Lob = Long Object
+    @Lob
+    @NotBlank(message = "Image Url must not be null, blank or empty")
+    private String imgUrl;
+
+    @Min(value = 1)
+    private float price;
+
+    public Pizza (){};
+
+    public List<SpecialOffer> getSpecialOffers() {
+        return this.specialOffers;
+    }
+
+    public void setSpecialOffers(List<SpecialOffer> specialOffers) {
+        this.specialOffers = specialOffers;
+    }
+
+    @OneToMany (mappedBy = "pizza")
+    private List<SpecialOffer> specialOffers;
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setNome(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImgUrl() {
+        return this.imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+
+    public float getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return this.name + " " + this.description + " " + this.price;
+    }
+
+}
